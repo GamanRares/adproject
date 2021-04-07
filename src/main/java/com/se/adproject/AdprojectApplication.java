0be34081d71af512d8630103982062951bd7f02b
ifzu <48160614+ifzu@users.noreply.github.com>
2021-04-07 23:53:58 +0300

    Test push

--- src/main/java/com/se/adproject/AdprojectApplication.java
+++ src/main/java/com/se/adproject/AdprojectApplication.java
@@ -3,6 +3,7 @@ package com.se.adproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test push
 @SpringBootApplication
 public class AdprojectApplication {
 
