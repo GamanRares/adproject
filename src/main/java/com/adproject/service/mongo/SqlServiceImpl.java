a0986e54db91bd05a25c24f4081048e53df045a8
ifzu <48160614+ifzu@users.noreply.github.com>
2021-05-18 11:03:07 +0300

    SQL queries except 7 and 10 + some generation methods + sql tests

---
+++ src/main/java/com/adproject/service/mongo/SqlServiceImpl.java
@@ -0,0 +1,102 @@
+package com.adproject.service.mongo;
+
+import com.adproject.db.datasource.TemplateFactory;
+import com.adproject.db.datasource.template.MySQLTemplate;
+import com.adproject.utils.ExecutionTime;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SqlServiceImpl {
+
+    //1. Get all students together with address, city, ...
+    public ExecutionTime executeQueryNo1() {
+        String query = "select * from student s, address a, city ct, country c " +
+                        "where s.address_id = a.id and a.city_id = ct.id and ct.country_id = c.id";
+        return timeOf(query);
+    }
+
+    //2. Get all students from a given city having a given teacher
+    public ExecutionTime executeQueryNo2() {
+        String query = "select * from student s, faculty_student fs, faculty f, faculty_teacher ft, teacher t where " +
+                       "s.forename = '2Ue' and s.id = fs.student_id and fs.faculty_id = f.id and f.id = ft.faculty_id and ft.teacher_id = t.id";
+        return timeOf(query);
+    }
+
+    //3.Get all students from a given city having a given teacher
+    public ExecutionTime executeQueryNo3() {
+        String query = "select s.id, s.surname, s.age from student s, address a, city c, faculty_student fs, faculty f, faculty_teacher ft, teacher t " +
+                "where c.name = 'GcC' and c.id = a.city_id and a.id = s.address_id " +
+                "and s.id = fs.student_id and fs.faculty_id = f.id " +
+                "and f.id = ft.faculty_id and ft.teacher_id = t.id and t.forename = '3nM' ";
+        return timeOf(query);
+    }
+
+    //4. Find which department from a given faculty has the most credits
+    public ExecutionTime executeQueryNo4() {
+        String query = "select max(the_sum), atb.the_id from (select d.id the_id, sum(s.credits) the_sum from department d, subject_department sd, subject s " +
+                "where d.id = sd.department_id and sd.subject_id = s.id group by d.id) as atb";
+        return timeOf(query);
+    }
+
+    //5. Find the university with the biggest number of students
+    public ExecutionTime executeQueryNo5() {
+        String query = "select max(numero), dummy.uname from (select count(s.id) numero, u.name uname from faculty f, faculty_student fs, student s, university u " +
+                "where f.university_id = u.id and f.id = fs.faculty_id and fs.student_id = s.id group by u.id) dummy";
+        return timeOf(query);
+    }
+
+    //6. Find the university with the biggest number of students at a course
+    public ExecutionTime executeQueryNo6() {
+        String query = "select max(numero), dummy.uname from (select count(s.id) numero, u.name uname from faculty f, faculty_student fs, student s, university u, department d, subject_department sd, subject ss " +
+                "where f.university_id = u.id and f.id = fs.faculty_id and fs.student_id = s.id and d.faculty_id = f.id and sd.department_id = d.id and sd.subject_id = ss.id and ss.name = '00M' group by u.id) dummy";
+        return timeOf(query);
+    }
+
+    //7. The oldest teacher from a country
+    public ExecutionTime executeQueryNo7() {
+        String query = "";
+        return timeOf(query);
+    }
+
+    //8. The faculty with the highest student age mean
+    public ExecutionTime executeQueryNo8() {
+        String query = "select dummy.facultyid, max(avv) from (select f.id facultyid, avg(s.age) avv from faculty f, faculty_student fs, student s " +
+                "where f.id = fs.faculty_id and fs.student_id = s.id group by f.id) dummy";
+        return timeOf(query);
+    }
+    //9. The students from a given country, studying a course with the least number of credits of that teacher
+    public ExecutionTime executeQueryNo9() {
+        String query = "select s.id from address a, city c, country cc, faculty f, faculty_student fs, student s, university u, department d, subject_department sd, subject ss, " +
+                "(select ss.id smallest, min(ss.credits) from teacher t, subject_teacher st, subject ss where  t.id = st.teacher_id and st.subject_id = ss.id and t.id = 3 group by t.id) dummy " +
+                "where s.address_id = a.id and a.city_id = c.id and c.country_id = cc.id " +
+                "and cc.id = 'some' and f.university_id = u.id " +
+                "and f.id = fs.faculty_id and fs.student_id = s.id and d.faculty_id = f.id and sd.department_id = d.id " +
+                "and sd.subject_id = ss.id and ss.name = dummy.smallest";
+        return timeOf(query);
+    }
+
+    //10. The students from a given country, studying a course with the least number of credits of that teacher
+    public ExecutionTime executeQueryNo10() {
+        String query = "";
+        return timeOf(query);
+    }
+
+    private ExecutionTime timeOf(String query){
+        long start = System.currentTimeMillis();
+        MySQLTemplate template = TemplateFactory.getTemplate(MySQLTemplate.class);
+        String analyzeQuery = "explain analyze " + query;
+        final String result = template.queryForObject(analyzeQuery, String.class);
+        long end = System.currentTimeMillis();
+        long dbExecutionTime = (long) dbTimeFromString(result);
+        return new ExecutionTime(end - start,dbExecutionTime);
+    }
+
+    private double dbTimeFromString(String result) {
+        String actual = "actual time=";
+        int startPos = result.indexOf(actual) + actual.length();
+        int endPos = result.indexOf("..");
+        String parsed = result.substring(startPos, endPos);
+        double time = Double.parseDouble(parsed);
+        return (time > 0.0d) ? time * 10: time * 1000;
+    }
+}
--- src/test/java/com/adproject/AdprojectApplicationTests.java
+++ src/test/java/com/adproject/AdprojectApplicationTests.java
@@ -4,6 +4,7 @@ import com.adproject.db.datasource.TemplateFactory;
 import com.adproject.db.datasource.entity.mongo.Student;
 import com.adproject.db.datasource.template.MySQLTemplate;
 import com.adproject.service.mongo.MongoServiceImpl;
+import com.adproject.service.mongo.SqlServiceImpl;
 import com.adproject.utils.ExecutionTime;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
+
+import static java.util.concurrent.ThreadLocalRandom.current;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -23,6 +28,9 @@ class AdprojectApplicationTests  {
 
     @Autowired
     MongoServiceImpl mongoService;
+
+    @Autowired
+    SqlServiceImpl sqlService;
 
     @Test
     void contextLoads() {
@@ -43,13 +51,32 @@ class AdprojectApplicationTests  {
     }
 
     @Test
+    void testSQLQueryNo1(){
+        ExecutionTime executionTime = sqlService.executeQueryNo1();
+        System.out.println(executionTime);
+    }
+
+    @Test
     void testMongoQueryNo2(){
         ExecutionTime executionTime = mongoService.executeQueryNo2();
         System.out.println(executionTime);
     }
+
+    @Test
+    void testSQLQueryNo2(){
+        ExecutionTime executionTime = sqlService.executeQueryNo2();
+        System.out.println(executionTime);
+    }
+
     @Test
     void testMongoQueryNo3(){
         ExecutionTime executionTime = mongoService.executeQueryNo3();
+        System.out.println(executionTime);
+    }
+
+    @Test
+    void testSQLQueryNo3(){
+        ExecutionTime executionTime = sqlService.executeQueryNo3();
         System.out.println(executionTime);
     }
 
@@ -60,18 +87,44 @@ class AdprojectApplicationTests  {
     }
 
     @Test
+    void testSQLQueryNo4(){
+        ExecutionTime executionTime = sqlService.executeQueryNo4();
+        System.out.println(executionTime);
+    }
+
+    @Test
     void testMongoQueryNo5(){
         ExecutionTime executionTime = mongoService.executeQueryNo5();
         System.out.println(executionTime);
     }
+
+    @Test
+    void testSQLQueryNo5(){
+        ExecutionTime executionTime = sqlService.executeQueryNo5();
+        System.out.println(executionTime);
+    }
+
     @Test
     void testMongoQueryNo6(){
         ExecutionTime executionTime = mongoService.executeQueryNo6();
         System.out.println(executionTime);
     }
+
+    @Test
+    void testSQLQueryNo6(){
+        ExecutionTime executionTime = sqlService.executeQueryNo6();
+        System.out.println(executionTime);
+    }
+
     @Test
     void testMongoQueryNo7(){
         ExecutionTime executionTime = mongoService.executeQueryNo7();
+        System.out.println(executionTime);
+    }
+
+    @Test
+    void testSQLQueryNo7(){
+        ExecutionTime executionTime = sqlService.executeQueryNo7();
         System.out.println(executionTime);
     }
 
@@ -82,14 +135,32 @@ class AdprojectApplicationTests  {
     }
 
     @Test
+    void testSQLQueryNo8(){
+        ExecutionTime executionTime = sqlService.executeQueryNo8();
+        System.out.println(executionTime);
+    }
+
+    @Test
     void testMongoQueryNo9(){
         ExecutionTime executionTime = mongoService.executeQueryNo9();
         System.out.println(executionTime);
     }
 
     @Test
+    void testSQLQueryNo9(){
+        ExecutionTime executionTime = sqlService.executeQueryNo9();
+        System.out.println(executionTime);
+    }
+
+    @Test
     void testMongoQueryNo10(){
         ExecutionTime executionTime = mongoService.executeQueryNo10();
+        System.out.println(executionTime);
+    }
+
+    @Test
+    void testSQLQueryNo10(){
+        ExecutionTime executionTime = sqlService.executeQueryNo10();
         System.out.println(executionTime);
     }
 
@@ -112,5 +183,25 @@ class AdprojectApplicationTests  {
     //TODO The students from a given country, studying a course with the least number of credits of that teacher
 
     //TODO The faculty with the least number of departments, but with the most students
+
+    //between(LocalDate.of(1989, Month.OCTOBER, 14), LocalDate.now()
+    public static LocalDate between(LocalDate startInclusive, LocalDate endExclusive) {
+        long startEpochDay = startInclusive.toEpochDay();
+        long endEpochDay = endExclusive.toEpochDay();
+        long randomDay = current()
+                .nextLong(startEpochDay, endEpochDay);
+
+        return LocalDate.ofEpochDay(randomDay);
+    }
+
+    static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static Random rnd = new Random();
+
+    static String randomString(int len){
+        StringBuilder sb = new StringBuilder(len);
+        for(int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        return sb.toString();
+    }
 
 }
